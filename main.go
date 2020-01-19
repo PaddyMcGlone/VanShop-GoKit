7ca8b7931b5006b82ea0138fbe1b1848ac0ddb8f
Paddy McGlone <paddymcglone@gmail.com>
2020-01-19 11:01:09 +0000

    Update main.go
    - Adding the Request and Response structs

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"strings"
+	"fmt"	
 )
 
 // Interface for the van service
 type VanService interface {
-	StockList() (Van, error)
-	Count() int
+	StockList(string) (Van, error)
+	Count(string) int
 }
 
 // Create a van object
@@ -24,21 +22,41 @@ type Van struct {
 // Implement the interface
 type vanService struct{}
 
-func (vanService) StockList() (Van, error) {
+func (vanService) StockList(s string) (Van, error) {
 	return Van {
 		make: "Volkswagen",
 		model: "T5",
 		length: "SWB",
 		engine: 2.0,
-		bhp: 140
-	}, nil
+		bhp: 140 }, nil
 }
 
-func (vanService) Count () int {
+func (vanService) Count (s string) int {
 	// Will eventually do a count to the db instance
-
 	return 1
 }
+
+
+
+// Adding the Request and Response structs
+// - Every method is modelled as an RPC call
+type stockListRequest struct {
+	S string `json:"s"`
+}
+
+type stockListResponse struct {
+	v Van `json:"v"`
+	Err string `json:"err,omitempty"`
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
 
 
 func main()  {
